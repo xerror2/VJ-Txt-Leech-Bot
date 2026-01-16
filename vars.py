b33f3767d9ad3327bd43f264ada5e1187c29c4a0
xerror2 <rachitkumargupta2008@gmail.com>
2026-01-17 00:58:56 +0530

    Update API_ID and API_HASH values in vars.py

--- vars.py
+++ vars.py
@@ -4,7 +4,8 @@
 
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "22182189"))
+API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61R29ae11960bf5")
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
+
 
